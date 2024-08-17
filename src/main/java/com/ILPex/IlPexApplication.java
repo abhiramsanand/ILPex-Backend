571e15ffa78984af56c5bec3934883ca368e64c5
abhiramsanand <abhiramcr7003@gmail.com>
2024-08-17 15:04:44 +0530

    first commit

---
+++ src/main/java/com/ILPex/IlPexApplication.java
@@ -0,0 +1,13 @@
+package com.ILPex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlPexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlPexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ILPex/IlPexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ILPex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlPexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
