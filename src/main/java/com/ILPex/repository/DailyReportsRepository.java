7b56de216008d98574136a39ef77ccab1378bb8a
Sreeson N <sreeson.n@experionglobal.local>
2024-08-19 15:31:08 +0530

    added repo

---
+++ src/main/java/com/ILPex/repository/DailyReportsRepository.java
@@ -0,0 +1,7 @@
+package com.ILPex.repository;
+
+import com.ILPex.entity.DailyReports;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DailyReportsRepository extends JpaRepository<DailyReports,Long> {
+}
