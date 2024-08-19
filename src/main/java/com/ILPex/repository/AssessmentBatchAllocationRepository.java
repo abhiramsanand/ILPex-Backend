4601a8605b0202cc8649dd9b60b6c376e575ddc3
abhiramsanand <abhiramcr7003@gmail.com>
2024-08-19 15:35:23 +0530

    done

--- src/main/java/com/ILPex/repository/AssessmentBatchAllocationRepository.java
+++ src/main/java/com/ILPex/repository/AssessmentBatchAllocationRepository.java
@@ -1,7 +1,6 @@
 package com.ILPex.repository;
 
 import com.ILPex.entity.AssessmentBatchAllocation;
-import com.ILPex.entity.Assessments;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AssessmentBatchAllocationRepository extends JpaRepository<AssessmentBatchAllocation, Long> {
