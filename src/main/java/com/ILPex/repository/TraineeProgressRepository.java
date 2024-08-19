8cbd312bbe6a036888d33a3e80b80d920b3e1a05
ashna-53 <ashnajamal104@gmail.com>
2024-08-19 15:31:28 +0530

    added trainee and traineeprogress repo

---
+++ src/main/java/com/ILPex/repository/TraineeProgressRepository.java
@@ -0,0 +1,6 @@
+package com.ILPex.repository;
+import com.ILPex.entity.TraineeProgress;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TraineeProgressRepository extends JpaRepository<TraineeProgress, Long> {
+}
---
+++ src/main/java/com/ILPex/repository/TraineesRepository.java
@@ -0,0 +1,7 @@
+package com.ILPex.repository;
+import com.ILPex.entity.Trainees;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TraineesRepository extends JpaRepository<Trainees, Long> {
+
+}
