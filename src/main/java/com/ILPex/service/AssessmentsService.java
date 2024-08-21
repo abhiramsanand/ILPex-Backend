1eebb2fb0596180c9b876e08a1b0050984c77015
athirark96 <athirabala10@gmail.com>
2024-08-21 12:30:17 +0530

    added assesssmentpage with name,count

---
+++ src/main/java/com/ILPex/service/AssessmentsService.java
@@ -0,0 +1,10 @@
+package com.ILPex.service;
+
+import com.ILPex.DTO.AssessmentDetailsDTO;
+
+import java.util.List;
+
+public interface AssessmentsService {
+
+    List<AssessmentDetailsDTO> getAssessmentDetails();
+}
