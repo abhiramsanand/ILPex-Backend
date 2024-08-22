36f12022871bd6e7f7a5bc1c10a14abd0c4ddfb2
abhiramsanand <abhiramcr7003@gmail.com>
2024-08-22 09:49:00 +0530

    origin

--- src/main/java/com/ILPex/constants/Message.java
+++ src/main/java/com/ILPex/constants/Message.java
@@ -5,5 +5,4 @@ public class Message {
     public static final String USER_CREATED_SUCCESSFULLY = "User created successfully.";
     public static final String USER_UPDATED_SUCCESSFULLY = "User updated successfully.";
     public static final String USER_DELETED_SUCCESSFULLY = "User deleted successfully.";
-
 }
