646efa47c46b3f8a8d8c2ea6f8fdda5f620a5a0c
ashna-53 <ashnajamal104@gmail.com>
2024-08-22 09:49:05 +0530

    Added api for assessmentfetch

--- src/main/java/com/ILPex/controller/AssessmentController.java
+++ src/main/java/com/ILPex/controller/AssessmentController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/assessments")
+@CrossOrigin(origins = "http://localhost:5174")
 public class AssessmentController {
 
     @Autowired
