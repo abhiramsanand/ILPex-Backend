c8738e68ac7ae7ba8a40f82f4da2a37c81c45999
ashna-53 <ashnajamal104@gmail.com>
2024-08-27 17:44:44 +0530

    Added average score with name api

--- src/main/java/com/ILPex/service/Impl/PercipioAssessmentServiceImpl.java
+++ src/main/java/com/ILPex/service/Impl/PercipioAssessmentServiceImpl.java
@@ -29,4 +29,20 @@ public class PercipioAssessmentServiceImpl implements PercipioAssessmentService 
 
         return averageScores;
     }
+
+    @Override
+    public Map<String, Double> getAverageScoresForAllTraineesWithName() {
+        List<PercipioAssessment> allAssessments = percipioAssessmentRepository.findAll();
+
+        // Group assessments by trainee name, filter out scores of zero, and calculate average score
+        Map<String, Double> averageScores = allAssessments.stream()
+                .filter(assessment -> assessment.getScore() > 0) // Exclude assessments with score of zero
+                .collect(Collectors.groupingBy(
+                        assessment -> assessment.getTrainees().getUsers().getUserName(), // Fetch trainee's name from Users entity
+                        Collectors.averagingDouble(PercipioAssessment::getScore)
+                ));
+
+        return averageScores;
+    }
+
 }
