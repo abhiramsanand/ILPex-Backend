6fc0e9886a4f77047279111b347beef2ef3bb33f
Sreeson N <sreeson.n@experionglobal.local>
2024-09-02 10:26:04 +0530

    added trainee id in daily report

--- src/main/java/com/ILPex/repository/TraineesRepository.java
+++ src/main/java/com/ILPex/repository/TraineesRepository.java
@@ -20,7 +20,7 @@ public interface TraineesRepository extends JpaRepository<Trainees, Long> {
             "FROM Trainees t JOIN t.users u WHERE t.batches.id = :batchId")
     List<TraineeDTO> findTraineesByBatchId(@Param("batchId") Long batchId);
 
-    @Query("SELECT new com.ILPex.DTO.TraineeDailyReportDTO(t.users.userName, " +
+    @Query("SELECT new com.ILPex.DTO.TraineeDailyReportDTO(t.id,t.users.userName, " +
             "(SELECT COUNT(c.id) FROM Courses c WHERE c.batch.id = b.id AND c.courseDate < CURRENT_DATE), " +
             "(SELECT COUNT(d.id) FROM DailyReports d WHERE d.trainees.batches.id = b.id AND d.trainees.users.id = t.users.id)) " +
             "FROM Trainees t " +
