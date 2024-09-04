c295d20cc0eb5027a957d683ded390b0c25dcf3d
abhiramsanand <abhiramcr7003@gmail.com>
2024-09-04 10:24:28 +0530

    done

--- src/main/java/com/ILPex/service/Impl/PercipioApiServiceImpl.java
+++ src/main/java/com/ILPex/service/Impl/PercipioApiServiceImpl.java
@@ -65,7 +65,7 @@ public class PercipioApiServiceImpl implements PercipioApiService {
 
         String body = """
                 {
-                             "start": "2024-09-02T23:25:24Z",
+                             "start": "2024-09-03T14:54:24Z",
                              "end": "2024-09-06T10:20:24Z",
                              "audience": "ALL",
                              "contentType": "Course,Linked Content,Scheduled Content,Assessment",
