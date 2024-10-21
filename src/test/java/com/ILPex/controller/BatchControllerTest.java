5969b3aad78b76a9779054075c4a98bb3486339a
abhiramsanand <abhiramcr7003@gmail.com>
2024-10-21 10:02:26 +0530

    build file

--- src/test/java/com/ILPex/controller/BatchControllerTest.java
+++ src/test/java/com/ILPex/controller/BatchControllerTest.java
@@ -47,7 +47,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-
 @SpringBootTest
 public class BatchControllerTest {
 
