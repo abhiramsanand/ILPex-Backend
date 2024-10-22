54f56c4948889639718751306134c43a9f0834ff
abhiramsanand <abhiramcr7003@gmail.com>
2024-10-22 11:32:08 +0530

    done

--- src/main/java/com/ILPex/controller/ProfileController.java
+++ src/main/java/com/ILPex/controller/ProfileController.java
@@ -1,26 +1,26 @@
-package com.ILPex.controller;
-
-import com.ILPex.dto.ProfileDTO;
-import com.ILPex.service.ProfileService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/api/v1/profiles")
-public class ProfileController {
-
-    @Autowired
-    private ProfileService profileService;
-
-    @GetMapping
-    public List<ProfileDTO> getAllProfiles() {
-        return profileService.getAllProfiles();
-    }
-
-    @GetMapping("/{traineeId}")
-    public ProfileDTO getProfileByTraineeId(@PathVariable Long traineeId) {
-        return profileService.getProfileByTraineeId(traineeId);
-    }
-}
+//package com.ILPex.controller;
+//
+//import com.ILPex.dto.ProfileDTO;
+//import com.ILPex.service.ProfileService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/api/v1/profiles")
+//public class ProfileController {
+//
+//    @Autowired
+//    private ProfileService profileService;
+//
+//    @GetMapping
+//    public List<ProfileDTO> getAllProfiles() {
+//        return profileService.getAllProfiles();
+//    }
+//
+//    @GetMapping("/{traineeId}")
+//    public ProfileDTO getProfileByTraineeId(@PathVariable Long traineeId) {
+//        return profileService.getProfileByTraineeId(traineeId);
+//    }
+//}
