8b7ece35900ec0a5fcc500ddd399e1c55b93a312
abhiramsanand <abhiramcr7003@gmail.com>
2025-01-27 12:35:26 +0530

    try#1

--- src/main/java/com/ILPex/service/Impl/ScheduledTaskServiceImpl.java
+++ src/main/java/com/ILPex/service/Impl/ScheduledTaskServiceImpl.java
@@ -28,7 +28,7 @@ public class ScheduledTaskServiceImpl implements ScheduledTaskService {
     private BatchRepository batchRepository;
 
     @Override
-    @Scheduled(fixedRate = 120000) // 2 minutes in milliseconds
+    @Scheduled(fixedRate = 180000) // 3 minutes in milliseconds
     public void fetchAndSaveData() {
         // Step 1: Generate request ID
         String requestId = percipioApiService.generateRequestId();
